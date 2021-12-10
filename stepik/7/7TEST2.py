20f8115bd0c4717504cffa2af5845aac62e8f2cc
Vadim Bykau <vadim.bykau@silkdata.ai>
2021-12-10 14:16:06 +0300

    add lessons from stepik

--- stepik/7/7TEST2.py
+++ stepik/7/7TEST2.py
@@ -1,2 +1,21 @@
-first_digit = str(35)
-print(first_digit[0]) #кол-во цифр в числе
+count = 0
+proizvedenie = 1
+for i in range(1, 11):
+    x = int(input())
+    if x >= 0:
+        proizvedenie *= x
+        count += 1
+if count > 0:
+    print(count)
+    print(proizvedenie)
+else:
+    print("NO")
+
+
+
+
+
+
+
+
+
